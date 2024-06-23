6c818ad6d9e7602415c92149b4c173ab3353253d
bdhamithkumara <damithkumararoxx55@gmail.com>
2024-06-23 23:15:00 +0530

    project

---
+++ components/PrincingSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrincingSection = () => {
+  return (
+    <div>PrincingSection</div>
+  )
+}
+
+export default PrincingSection
---
+++ public/images/index.js
@@ -0,0 +1,13 @@
+import AboutSectionImage from './App mockups.svg'
+import FeaturesSectionImageOne from './Tela 1.svg'
+import FeaturesSectionImageTwo from './Tela 2.svg'
+import FeaturesSectionImageThree from './Tela 3.svg'
+import DownloadSectionImage from './rapaz-ouvindo-musica-com-fones-de-ouvido-e-cantando-no-aplicativo-de-karaoke-removebg 1 (1).svg'
+
+export {
+    AboutSectionImage,
+    FeaturesSectionImageOne,
+    FeaturesSectionImageTwo,
+    FeaturesSectionImageThree,
+    DownloadSectionImage
+}
